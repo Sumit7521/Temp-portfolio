1a41c96cfb67b9e79f0ac0efbecd7f744a483666
SUMIT SHARMA <sumit140803@gmail.com>
2025-10-30 03:50:02 +0530

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to your actual repository name
+  base: '/', // Change this to your actual repository name
 });
